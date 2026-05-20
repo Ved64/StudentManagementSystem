86ed398a9195b4f1b8b255aee47cfb76ad5ba4f9
Ved64 <vedantkhadse2005@gmail.com>
2026-05-20 15:10:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from student import student_info
+from marks import student_marks
+
+print("===================================")
+print("   STUDENT MANAGEMENT SYSTEM")
+print("===================================")
+
+# Calling functions from modules
+student_info()
+student_marks()
+
+print("===================================")
+print(" Program Executed Successfully ")
+print("===================================")
