013031c537b7b63efb0304469f07689f21dfefcc
safwannadkar13 <safwannadkar13@gmail.com>
2026-05-20 15:17:15 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,12 @@
+def student_info():
+    print("\n--- Student Information Module ---")
+
+    name = input("Enter Student Name: ")
+    roll_no = input("Enter Roll Number: ")
+    branch = input("Enter Branch: ")
+
+    print("\nStudent Details")
+    print("----------------------")
+    print(f"Name      : {name}")
+    print(f"Roll No   : {roll_no}")
+    print(f"Branch    : {branch}")
